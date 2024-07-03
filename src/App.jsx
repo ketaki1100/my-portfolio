a07ed9ac50f6c93c4a865271828b3b40b3816e70
Ketaki <125079298+ketaki1100@users.noreply.github.com>
2024-07-03 13:53:12 +0000

    final

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Ketaki Aloni",
+  title: "Software Engineering Student",
+  email: "ketakialoni@gmail.com",
+  gitHub: "ketaki1100",
+  instagram: "kx_art7",
+  linkedIn: "ketaki-aloni",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
